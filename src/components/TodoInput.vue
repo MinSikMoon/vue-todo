0a878fab41f451f5d5a4bfe27c324011a16e5fb6
minsikmoon <msmoon.business@gmail.com>
2018-12-30 19:17:00 +0900

    list 만들기1

--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="inputBox shadow">
-    <input type="text" placeholder="type what you have to do" 
-    v-model="newTodoItem" v-on:keyup.enter="addTodo">
+    <input
+      type="text"
+      placeholder="type what you have to do"
+      v-model="newTodoItem"
+      v-on:keyup.enter="addTodo"
+    >
     <span class="addContainer" v-on:click="addTodo">
-        <i class="addBtn fa fa-plus"></i>
+      <i class="addBtn fa fa-plus"></i>
     </span>
   </div>
 </template>
@@ -17,19 +21,43 @@ export default {
   },
 
   methods: {
-      addTodo(){
-          if(this.newTodoItem != ""){
-              var value = this.newTodoItem && this.newTodoItem.trim();
-              localStorage.setItem(value, value);
-              this.clearInput();
-          }
-      },
-      clearInput() {
-          this.newTodoItem = '';
+    addTodo() {
+      if (this.newTodoItem != "") {
+        var value = this.newTodoItem && this.newTodoItem.trim();
+        localStorage.setItem(value, value);
+        this.clearInput();
       }
+    },
+    clearInput() {
+      this.newTodoItem = "";
+    }
   }
 };
 </script>
 
-<style>
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478fb, #8763fb);
+  display: inline-block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
 </style>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,10 +1,23 @@
 <template>
-    <div>list</div>
+    <section>
+       <li v-for="todoItem in todoItems">{{todoItem}}</li>
+    </section>
 </template>
 
 <script>
 export default {
-    
+    data(){
+        return{
+            todoItems: []
+        }
+    },
+    created(){
+        if(localStorage.length > 0){
+            for(var i=0; i<localStorage.length; i++){
+                this.todoItems.push(localStorage.key(i));
+            }
+        }
+    }
 }
 </script>
 
