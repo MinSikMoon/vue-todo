f00d0763256182ff3f55b24410baf020e36b3f15
minsikmoon <msmoon.business@gmail.com>
2018-12-30 20:04:36 +0900

    list 스타일

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,14 @@
 <template>
     <section>
-       <li v-for="todoItem in todoItems">{{todoItem}}</li>
+        <ul>
+            <li v-for="todoItem in todoItems" class="shadow">
+                <i class="checkBtn fa fa-check" aria-hidden="true"></i>
+                {{todoItem}}
+                <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                    <i class="fa fa-trash-o" aria-hidden="true"></i>
+                </span>
+            </li>
+        </ul>
     </section>
 </template>
 
@@ -14,7 +22,10 @@ export default {
     created(){
         if(localStorage.length > 0){
             for(var i=0; i<localStorage.length; i++){
-                this.todoItems.push(localStorage.key(i));
+                var tempItem = localStorage.key(i).trim();
+                if(tempItem != null || tempItem.length != 0){
+                    this.todoItems.push(tempItem);
+                }
             }
         }
     }
@@ -22,5 +33,33 @@ export default {
 </script>
 
 <style>
-    
+    ul{
+        list-style-type: none;
+        padding-left: 0px;
+        margin-top: 0;
+        text-align: left;
+    }
+
+    li{
+        display: flex;
+        min-height: 50px;
+        height: 50px;
+        line-height: 50px;
+        margin: 0.5rem 0;
+        padding: 0 0.9rem;
+        background: white;
+        border-radius: 5px;
+    }
+
+    .checkBtn{
+        line-height: 45px;
+        color: #62acde;
+        margin-right: 5px;
+    }
+
+    .removeBtn{
+        margin-left: auto;
+        color: rgba(236, 82, 82, 0.966);
+    }
+
 </style>
